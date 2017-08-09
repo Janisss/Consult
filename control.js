a79589142045469b86131f32cd8c8b878f098cc8
soltesyzak <30795151+soltesyzak@users.noreply.github.com>
2017-08-09 11:55:38 +0200

    menu

---
+++ control.js
@@ -0,0 +1,64 @@
+		//SKRIPTY NA PRECHODY PRVEJ KATEGORIE V MENU
+		$(".cubeA").click(function(){
+			$(".content").addClass("active");
+			$(".content").empty();
+			setTimeout(function(){
+				$.ajax('menu/content-a.html', {
+					success: function(response) {
+						$(".content").html(response);
+					}
+				});//ajax
+			}, 400);
+		});
+		$(".cubeB").click(function(){
+			$(".content").addClass("active");
+			$(".content").empty();
+			setTimeout(function(){
+				$.ajax('menu/content-b.html', {
+					success: function(response) {
+						$(".content").html(response);
+					}
+				});//ajax
+			}, 400);
+		});		
+		$(".cubeC").click(function(){
+			$(".content").addClass("active");
+			$(".content").empty();
+			setTimeout(function(){
+				$.ajax('menu/content-c.html', {
+					success: function(response) {
+						$(".content").html(response);
+					}
+				});//ajax
+			}, 400);
+		});
+		$(".cubeD").click(function(){
+			$(".content").addClass("active");
+			$(".content").empty();
+			setTimeout(function(){
+				$.ajax('menu/content-d.html', {
+					success: function(response) {
+						$(".content").html(response);
+					}
+				});//ajax
+			}, 400);
+		});		
+		
+		$(".cubeB").click(function(){
+			$(".content").addClass("active");
+		});
+		
+		$(".cubeC").click(function(){
+			$(".content").addClass("active");
+		});
+		
+		$(".cubeD").click(function(){
+			$(".content").addClass("active");
+		});
+		
+		$(".home").click(function(){
+			$(".content").removeClass("active");
+			$(".content").empty();
+		});
+		//KONIEC PRVEJ KATEGORIE
+		
